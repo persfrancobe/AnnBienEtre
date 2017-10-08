0eb9bd49d8deceb36acd5f95ec39420a76bbaae2
kalantarian_ehsan <persfrancobe@gmail.com>
2017-10-08 22:51:47 +0200

    creation providers and promotions and categories band courses (page index)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
 
         ];
 
