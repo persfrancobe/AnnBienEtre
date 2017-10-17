78782e2e2c9c6975349e4843f5c8792c9371e15f
kalantarian_ehsan <persfrancobe@gmail.com>
2017-10-17 18:42:01 +0200

    homepage,provider,providers,category,categories,course,courses,promotions,promotion

--- src/AppBundle/Controller/FrontEnd/CourseController.php
+++ src/AppBundle/Controller/FrontEnd/CourseController.php
@@ -24,11 +24,16 @@ class CourseController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
+        $providers = $em->getRepository('AppBundle:Provider')->findAll();
+        $promotions = $em->getRepository('AppBundle:Promotion')->findAll();
         $courses = $em->getRepository('AppBundle:Course')->findAll();
+        $cities = $em->getRepository('AppBundle:City')->findAll();
+        $service_categories = $em->getRepository('AppBundle:ServiceCategory')->findAll();
+        /* $slider = $em->getRepository('AppBundle:Images')->findBy(array('type' => 'slider'));*/
 
-        return $this->render('frontEnd/courses/index.html.twig', array(
-            'courses' => $courses,
-        ));
+
+        return $this->render('frontEnd/courses/index.html.twig', array('cities' => $cities, 'providers' => $providers, 'promotions' => $promotions,
+            'courses' => $courses, 'service_categories' => $service_categories/*, 'sliders' => $slider*/));
     }
 
     /**
