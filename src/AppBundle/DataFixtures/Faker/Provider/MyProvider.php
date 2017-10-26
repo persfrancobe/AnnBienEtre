de3db9df6a0ebae93aa94224f620f061dcfadb56
kalantarian_ehsan <persfrancobe@gmail.com>
2017-10-27 00:08:00 +0200

    last work

---
+++ src/AppBundle/DataFixtures/Faker/Provider/MyProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\DataFixtures\Faker\Provider;
+
+class MyProvider extends \Faker\Provider\Address
+{
+    public static function newsName()
+    {
+    return;
+    }
+}
